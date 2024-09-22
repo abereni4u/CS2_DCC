a9028dcf51311092763c5dca41c75101c82b91a3
Abereni Opuiyo <aberenijopuiyo@gmail.com>
2024-09-22 13:03:42 -0400

    Create RightTriangle class

---
+++ Projects/Program2/src/RightTriangle.java
@@ -0,0 +1,16 @@
+/**
+ * RightTriangle is a concrete class that represents a two-dimensional
+ * RightTriangle shape. It inherits directly from the Triangle abstract class.
+ */
+public class RightTriangle extends Triangle{
+
+
+	// --------------------------//
+	// CONSTRUCTORS
+	// --------------------------//
+	
+
+	// --------------------------//
+	// METHODS
+	// --------------------------//
+}
