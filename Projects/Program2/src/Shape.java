5dcc75b3c4b224ca6cd30cb75dec6e89664e878d
Abereni Opuiyo <aberenijopuiyo@gmail.com>
2024-09-22 11:35:38 -0400

    Implement Shape class methods

--- Projects/Program2/src/Shape.java
+++ Projects/Program2/src/Shape.java
@@ -1,3 +1,8 @@
+/**
+ * The Shape class represents an “undifferentiated” or 
+ * “unspecified” 2D Shape.  It is an abstract class that 
+ * partially implements the Boundable interface. 
+ */
 public abstract class Shape implements Boundable{
 	
 	
@@ -35,4 +40,58 @@ public abstract class Shape implements Boundable{
 		this.boundingbox = new BoundingBox(size, size);
 	}
 	
+	//--------------------------//
+	// METHODS
+	//--------------------------//
+	
+	/**
+	 * Returns the value of the boundingBox field.
+	 * @return boundingBox of the Shape instance.
+	 */
+	public BoundingBox getBoundingBox(){
+		return this.boundingbox;
+	}
+
+	/**
+	 * An abstract method that returns the name of the kind of shape.
+	 * @return The name of the kind of shape.
+	 */
+	public abstract String getName();
+
+
+	/**
+	 * Returns the value returned by the boundingBox field’s 
+	 * getWidth() method.
+	 * @return the width of the shape.
+	 */
+	public double width(){
+		return this.boundingbox.getWidth();	
+	}
+
+	/**
+	 * Returns the value returned by the boundingBox field's
+	 * getHeight() method.
+	 * @return the height of the shape.
+	 */
+	public double height(){
+		return this.boundingbox.getHeight();
+	}
+
+	/**
+	 * This method will return a well-formatted String 
+	 * object to be used in the shape analysis report.
+	 *
+	 * @return
+	 */
+	public String toString(){
+		StringBuilder shapeReport = new StringBuilder();
+
+		shapeReport.append(String.format("\n%-20s\n", this.getName()));
+		shapeReport.append(String.format("	width: %-20.2f\n",this.width())); 
+		shapeReport.append(String.format("	height: %-20.2f\n",this.height()));
+		shapeReport.append(String.format("	area: %-20.2f\n",this.area()));
+		shapeReport.append(String.format("	perimeter: %-20.2f\n",this.perimeter()));
+		return shapeReport.toString();
+	}
+
 }
--- Projects/Program2/src/testingHere.java
+++
@@ -1,8 +0,0 @@
-public class testingHere {
-
-	public static void main(String[] args) {
-		BoundingBox myBox = new BoundingBox(12.0, 5.0);
-		System.out.println("\nIs this working: " + myBox.getMinDum());
-	}
-
-}
