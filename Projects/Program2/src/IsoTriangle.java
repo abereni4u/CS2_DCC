6ee5f699e627f29a815cd1750cde4f41b3b7faa6
Abereni Opuiyo <aberenijopuiyo@gmail.com>
2024-09-22 13:21:03 -0400

    Implement construcotrs for IsoTriangle class

--- Projects/Program2/src/IsoTriangle.java
+++ Projects/Program2/src/IsoTriangle.java
@@ -1,9 +1,41 @@
 /**
  * IsoTriangle is a concrete class that represents a two dimensional
- * IsocelesTriangle shape. It inherits directly from the Triangle abstract class.
+ * IsocelesTriangle shape. It inherits directly from the Triangle abstract
+ * class.
  */
-public class IsoTriangle extends RightTriangle{
+public class IsoTriangle extends RightTriangle {
 
+	// --------------------------//
+	// CONSTRUCTORS
+	// --------------------------//
 
+	/**
+	 * No-arg constructor for IsoTriangle.
+	 */
+	public IsoTriangle() {
+		super();
+	}
 
+	/**
+	 * IsoTriangle single arg constructor.
+	 * 
+	 * @param size size of the triangle.
+	 */
+	public IsoTriangle(double size) {
+		super(size);
+	}
+
+	/**
+	 * IsoTriangle constructor that takes width and height as arguments.
+	 * 
+	 * @param width  width of IsoTriangle.
+	 * @param height height of IsoTriangle.
+	 */
+	public IsoTriangle(double width, double height) {
+		super(width, height);
+	}
+
+	// --------------------------//
+	// METHODS
+	// --------------------------//
 }
