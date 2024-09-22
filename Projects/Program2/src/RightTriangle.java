2741ba2b2a7629a807fe82ea3c45f53cd162ab79
Abereni Opuiyo <aberenijopuiyo@gmail.com>
2024-09-22 13:08:12 -0400

    Implement constructors for RightTriangle class

--- Projects/Program2/src/RightTriangle.java
+++ Projects/Program2/src/RightTriangle.java
@@ -9,7 +9,31 @@ public class RightTriangle extends Triangle{
 	// CONSTRUCTORS
 	// --------------------------//
 	
+	/**
+	 * No-arg constructor for RightTriangle.
+	 */
+	public RightTriangle(){
+		super();
+	}
 
+	/**
+	 * Constructor taking a single size argument for RightTriangle.
+	 * @param size 
+	 */
+	public RightTriangle(double size){
+		super(size);
+	}
+
+	/**
+	 * Constructor taking width and height arguments for RightTriangle.
+	 * @param width width of RightTriangle.
+	 * @param height height of RightTriangle.
+	 */
+	public RightTriangle(double width, double height){
+		super(width, height);
+	}
+
+	
 	// --------------------------//
 	// METHODS
 	// --------------------------//
