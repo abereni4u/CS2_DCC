116676024baa52f9e71c323675f89d0eb8ec880f
Abereni Opuiyo <aberenijopuiyo@gmail.com>
2024-09-22 13:32:03 -0400

    Implement methods for IsoTriangle

--- Projects/Program2/src/IsoTriangle.java
+++ Projects/Program2/src/IsoTriangle.java
@@ -38,4 +38,28 @@ public class IsoTriangle extends RightTriangle {
 	// --------------------------//
 	// METHODS
 	// --------------------------//
+
+	/**
+	 * Returns the perimeter of the IsoTriangle based on the following 
+	 * equation: perimeter = width + 2 (sqrt((width / 2)^2) + (height^2)).
+	 */
+	public double perimeter(){
+		
+		// Set get width and height of triangle.
+		double w = this.width();
+		double h = this.height();
+		
+		// Inside square root: divide width by 2, square it and add it 
+		// to height, squared.
+		double w2 = w/2;
+		double len = Math.sqrt(w2*w2 + h*h);
+
+		double perimeter = w + (2 * len);
+		return perimeter;
+	}
+
+	@Override
+	public String getName(){
+		return "IsoTriangle";
+	}
 }
