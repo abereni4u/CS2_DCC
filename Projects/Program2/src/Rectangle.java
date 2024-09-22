8d6539d16730dad02c1e7a7da8e67059fe1253be
Abereni Opuiyo <aberenijopuiyo@gmail.com>
2024-09-22 11:54:55 -0400

    Create Rectangle constructors

--- Projects/Program2/src/Rectangle.java
+++ Projects/Program2/src/Rectangle.java
@@ -4,4 +4,37 @@
  */
 public class Rectangle extends Shape{
 		
+	//--------------------------//
+	// CONSTRUCTORS
+	//--------------------------//
+	
+	
+	/**
+	 * No-arg constructor for Rectangle 
+	 */
+	public Rectangle(){
+		super();
+	}
+	
+	/**
+	 * Rectangle constructor taking a single size argument for both the width and 
+	 * height.
+	 * @param size width and height of the Rectangle.
+	 */
+	public Rectangle(double size){
+		super(size, size);	
+	}
+
+	/**
+	 * Rectangle constructor taking width and height arguments.
+	 * @param width width of the Rectangle.
+	 * @param height height of the Rectangle.
+	 */
+	public Rectangle(double width, double height){
+		super(width, height);
+	}
+	
+	//--------------------------//
+	// METHODS
+	//--------------------------//
 }
