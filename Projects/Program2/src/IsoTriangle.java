387527a3f20da295dcd0a246024531db96147f1c
Abereni Opuiyo <aberenijopuiyo@gmail.com>
2024-09-22 13:16:16 -0400

    Create IsoTriangle class

---
+++ Projects/Program2/src/IsoTriangle.java
@@ -0,0 +1,9 @@
+/**
+ * IsoTriangle is a concrete class that represents a two dimensional
+ * IsocelesTriangle shape. It inherits directly from the Triangle abstract class.
+ */
+public class IsoTriangle extends RightTriangle{
+
+
+
+}
